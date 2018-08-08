6ffc2ffba8a2c34a9be001eeda2a9c4814c1e6b8
Pianist038801 <maximivanov818@gmail.com>
2018-08-08 05:26:19 +0400

    add: update_referral_UI

--- source/components/modal-component/detail-referral.vue
+++ source/components/modal-component/detail-referral.vue
@@ -2,94 +2,82 @@
     modal(ref="modalphone")
         .modal__content
             .modal__content-row
-                
-                    .modal-appointment__title
-                        .title.mod--modal-appointment Search Patient
- 
-                        .modal-content-row
-  
-                            .modal-appointment__info-lang
-                                multiselect(
-                                v-model="phoneNumber",
-                                :options="['Search All', 'Search EPIC', 'Search CERNER']",
-                                @input="",
-                                :searchable="false",
-                                :allowEmpty="false",
-                                :showLabels="false",
-                                placeholder="Select EHR"
-                                ).ui-multiselect.ui-multiselect--default
- 
+                .modal-appointment__title
+                    .title.mod--modal-appointment Patient Referral Details 
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Referral Id    
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname External Referral ID
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Referral Auth Start Date
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Referral Auth End Date
+                            input(:value="$root._data.callerPhone" readonly).infovalue
 
-                            .modal-appointment__info-lang
-                                multiselect(
-                                v-model="searchType",
-                                :options="['Patient MRN', 'Patient First Name', 'Patient Last Name', 'Patient DOB','Patient Zip', 'Patient Phone Number', 'Patient SSN']",
-                                @input="showType",
-                                :searchable="false",
-                                :allowEmpty="false",
-                                :showLabels="false",
-                                placeholder="Select Criteria"
-                                ).ui-multiselect.ui-multiselect--default
-                            input(v-model="searchTemp").modal-appointment__info-lang
-                            a(href="#3", @click="searchKey=searchTemp==''?-1:searchTemp").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Search
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Patient Id    
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Patient Name
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Patient Address
+                            input(:value="$root._data.callerPhone" readonly).infovalue
 
-            table.search-patient__table
-                tr
-                    th.g-align-center MRN
-                    th.g-align-center First Name
-                    th.g-align-center Last Name
-                    th.g-align-center DOB
-                    th.g-align-center ZIP
-                    th.g-align-center Phone Number
-                    th.g-align-center SSN
-                tr(v-for="(patient, index) in $root._data.Patients",v-bind:style="{color: patient==chosen?'red':'black'}", v-if="ifShow(patient)==true" @click="clickRow(patient)")
-                
-                    td().g-align-center {{patient.MRN}}
-                    td.g-align-center {{patient.Name.split(' ')[1]}}
-                    td.g-align-center {{patient.Name.split(' ')[0]}}
-                    td.g-align-center {{patient.DateOfBirth.split(' ')[0]}}
-                    td.g-align-center Zip
-                    td.g-align-center {{patient.PhoneNumber}}
-                    td.g-align-center {{patient.SSN}}
-                //- tr(v-for="(item, index) in existingApointmentSlots", :class="{'state--hold': item.Status === 'On Hold' }").make-appointment__table-tr
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Referral Type
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Referral Status
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Diagnoses
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname procedures
+                            input(:value="$root._data.callerPhone" readonly).infovalue
 
-                //-     td.make-appointment__table-time {{item.StartTime | moment("HH:mm")}} - {{item.EndTime | moment("HH:mm A")}}
-                //-     td {{item.Name}}
-                //-     td.g-align-center {{item.Facility}}
-                //-     td.g-align-center {{item.Provider}}
-                //-     td.g-align-center {{item.Physician}}
-                //-     td.make-appointment__table-status.g-align-center {{item.Status}}
-                //-     td.g-align-center.make-appointment__table-action(v-if="item.Status === 'On Hold'")
-                //-         a(href="#3", @click.prevent="").ui-btn.ui-btn--skin-default.ui-btn--theme-disable-border.mod--block update
-                //-     td.g-align-center.make-appointment__table-action(v-else)
-                //-         a(href="#3", @click.prevent="showModal(index)").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.mod--block book now
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Referred by provider name
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Referred to provider name
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Referred to provider address
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                        .referral__detail-field
+                            span().infoname Referred to facility address
+                            input(:value="$root._data.callerPhone" readonly).infovalue
+                                 
             .modal-appointment__row
-                    a(href="#3", @click="onSelect").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Select
+                    a(href="#3", @click="onClose").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Ok
 
 </template>
-<script> 
+<script>
 
     import modal from "./modal.vue";
     import Multiselect from 'vue-multiselect';
-    import Vue from 'vue'; 
+    import Vue from 'vue';
     import axios from 'axios';
     import store from '../../js/store.js';
 
     export default {
         props: ['show'],
-        components: { 
+        components: {
             modal,
             Multiselect,
         },
         methods: {
-            onSelect(){
+            onClose(){
                 this.$refs.modalphone.close()
-                console.log('THIS.CHOSEN=');
-                console.log(this.chosen);
-                if(this.chosen.Name=='Grace Paz')
-                    store.dispatch('setId', 1)
-                else
-                    store.dispatch('setId', 0)
             },
             ifShow(patient){
                 console.log('searchsType')
--- source/components/modal-component/update-referral.vue
+++ source/components/modal-component/update-referral.vue
@@ -2,94 +2,104 @@
     modal(ref="modalphone")
         .modal__content
             .modal__content-row
-                
-                    .modal-appointment__title
-                        .title.mod--modal-appointment Search Patient
- 
-                        .modal-content-row
-  
-                            .modal-appointment__info-lang
-                                multiselect(
-                                v-model="phoneNumber",
-                                :options="['Search All', 'Search EPIC', 'Search CERNER']",
-                                @input="",
+                .modal-appointment__title
+                    .title.mod--modal-appointment Patient Referral Details 
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname STATUS    
+                            multiselect(
+                                :options="['Denied', 'Pending', 'Closed', 'Authorized']",
                                 :searchable="false",
+                                v-model="status",
                                 :allowEmpty="false",
-                                :showLabels="false",
-                                placeholder="Select EHR"
-                                ).ui-multiselect.ui-multiselect--default
- 
+                                :showLabels="false"
+                            ).ui-multiselect.ui-multiselect--default.inline-block
+                        .referral__detail-field
+                            span().infoname Reason
+                            input(v-model="reason").infovalue
+                         
 
-                            .modal-appointment__info-lang
-                                multiselect(
-                                v-model="searchType",
-                                :options="['Patient MRN', 'Patient First Name', 'Patient Last Name', 'Patient DOB','Patient Zip', 'Patient Phone Number', 'Patient SSN']",
-                                @input="showType",
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Referral Start Date
+                            input(v-model="startDate").infovalue
+                        .referral__detail-field
+                            span().infoname Referral Expiration Date
+                            input(v-model="expirationDate").infovalue
+                        .referral__detail-field
+                            span().infoname Schedule By Date
+                            input(v-model="scheduleDate").infovalue
+                        .referral__detail-field
+                            span().infoname Scheduling Status
+                            input(v-model="scheduleStatus").infovalue
+
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Referred to Dept ID
+                            input(v-model="deptID").infovalue
+                        .referral__detail-field
+                            span().infoname Referred to Dept ID Type
+                            input(v-model="deptType").infovalue
+                        .referral__detail-field
+                            span().infoname Referred to Location ID
+                            input(v-model="locationID").infovalue
+                        .referral__detail-field
+                            span().infoname Referred to Location ID Type
+                            input(v-model="locationType").infovalue
+
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Referred by provider ID Type
+                            input(v-model="providerType").infovalue
+                        .referral__detail-field
+                            span().infoname Referred to provider ID
+                            input(v-model="providerID").infovalue
+                        .referral__detail-field
+                            span().infoname Assign Referral to Appointment
+                            
+                            input(type="checkbox" ).infovalue
+                            multiselect(
+                                :options="['Denied', 'Pending', 'Closed', 'Authorized']",
                                 :searchable="false",
+                                v-model="status",
                                 :allowEmpty="false",
-                                :showLabels="false",
-                                placeholder="Select Criteria"
-                                ).ui-multiselect.ui-multiselect--default
-                            input(v-model="searchTemp").modal-appointment__info-lang
-                            a(href="#3", @click="searchKey=searchTemp==''?-1:searchTemp").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Search
+                                :showLabels="false"
+                            ).ui-multiselect.ui-multiselect--default.inline-block
+                        .referral__detail-field
+                            span().infoname Unassign Referral to Appointment
+                            input(type="checkbox" ).infovalue
 
-            table.search-patient__table
-                tr
-                    th.g-align-center MRN
-                    th.g-align-center First Name
-                    th.g-align-center Last Name
-                    th.g-align-center DOB
-                    th.g-align-center ZIP
-                    th.g-align-center Phone Number
-                    th.g-align-center SSN
-                tr(v-for="(patient, index) in $root._data.Patients",v-bind:style="{color: patient==chosen?'red':'black'}", v-if="ifShow(patient)==true" @click="clickRow(patient)")
-                
-                    td().g-align-center {{patient.MRN}}
-                    td.g-align-center {{patient.Name.split(' ')[1]}}
-                    td.g-align-center {{patient.Name.split(' ')[0]}}
-                    td.g-align-center {{patient.DateOfBirth.split(' ')[0]}}
-                    td.g-align-center Zip
-                    td.g-align-center {{patient.PhoneNumber}}
-                    td.g-align-center {{patient.SSN}}
-                //- tr(v-for="(item, index) in existingApointmentSlots", :class="{'state--hold': item.Status === 'On Hold' }").make-appointment__table-tr
-
-                //-     td.make-appointment__table-time {{item.StartTime | moment("HH:mm")}} - {{item.EndTime | moment("HH:mm A")}}
-                //-     td {{item.Name}}
-                //-     td.g-align-center {{item.Facility}}
-                //-     td.g-align-center {{item.Provider}}
-                //-     td.g-align-center {{item.Physician}}
-                //-     td.make-appointment__table-status.g-align-center {{item.Status}}
-                //-     td.g-align-center.make-appointment__table-action(v-if="item.Status === 'On Hold'")
-                //-         a(href="#3", @click.prevent="").ui-btn.ui-btn--skin-default.ui-btn--theme-disable-border.mod--block update
-                //-     td.g-align-center.make-appointment__table-action(v-else)
-                //-         a(href="#3", @click.prevent="showModal(index)").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.mod--block book now
+                    .referral-view__top
+                        .referral__detail-field
+                            span().infoname Notes
+                            textarea(v-model="notes").infovalue
+                        .referral__detail-field 
+                            span().infoname Comments
+                            textarea(v-model="comments").infovalue
+                                 
+            
             .modal-appointment__row
-                    a(href="#3", @click="onSelect").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Select
+                    a(href="#3", @click="onClose").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Close
+                    a(href="#3", @click="onClose").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Update
 
 </template>
-<script> 
+<script>
 
     import modal from "./modal.vue";
     import Multiselect from 'vue-multiselect';
-    import Vue from 'vue'; 
+    import Vue from 'vue';
     import axios from 'axios';
     import store from '../../js/store.js';
 
     export default {
         props: ['show'],
-        components: { 
+        components: {
             modal,
             Multiselect,
         },
         methods: {
-            onSelect(){
+            onClose(){
                 this.$refs.modalphone.close()
-                console.log('THIS.CHOSEN=');
-                console.log(this.chosen);
-                if(this.chosen.Name=='Grace Paz')
-                    store.dispatch('setId', 1)
-                else
-                    store.dispatch('setId', 0)
             },
             ifShow(patient){
                 console.log('searchsType')
@@ -137,23 +147,6 @@
                     phantom3 : ""
                 }
                 console.log(results)
-                axios({method: 'post',
-                    url: 'http://office.healthcareintegrations.com:8900/setTempDNIS',
-                    responseType: 'json',
-                    data: results}
-                )
-                .then(function(response){
-                    console.log('Axios_Response=', response);
-                    if(response.data.error){
-                        alert('No Available TempDNIS Now.');
-                    }
-                    else{
-                        vm.tempDNIS = response.data.tempDNIS
-                        console.log('TEMPDNIS=', vm.tempDNIS);
-                        //const callNumber = 1111111; // get from finesse api
-                        //const transferNumber = results.destinationNo.split('-').join('')
-                    }
-                });
             },
             clickRow(item){
                 console.log(item)
@@ -162,13 +155,22 @@
         },        
         data() {
             return {
-                phoneNumber: '',
-                searchType: '',
-                phoneNote: '',
-                searchKey: '',
-                searchTemp: '',
-                showRecent: true,
-                chosen: null,
+                status: '',
+                reason: 'Reason',
+                startDate: '',
+                expirationDate: '',
+                scheduleDate: '',
+                scheduleStatus: '',
+                deptID: '',
+                deptType: '',
+                locationID: '',
+                locationType: '',
+                providerID: '',
+                providerType: '',
+                checkAppointment: true,
+                assignedAppointment: '',
+                notes: '',
+                comments: '',
             }
         },
          
@@ -187,7 +189,7 @@
             vm.phoneNumber='';
         },
         computed:{
-             
+            //console.log('React Bootstrapy', let 'v', custom 'cus')
         },
         beforeDestroy() {
         },
